24a1adfa656a8fb1c06ecfc1ef0c2e09c5bc9832
Théo <theo.delas@ynov.com>
2019-12-02 11:18:39 +0100

    ajouter une classe, modifier une classe

--- Laravel_Game/app/Http/Controllers/Admin/ClasseController.php
+++ Laravel_Game/app/Http/Controllers/Admin/ClasseController.php
@@ -28,18 +28,46 @@ class ClasseController extends Controller
      */
     public function create()
     {
-        //
+        // retourne ma vue de création de personnage avec comme parametre mes classe stockées dans la variable $classe
+        return view ('admin.classe.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return Response
      */
     public function store(Request $request)
     {
-        //
+        // ma requete a besoin de ces valeurs pour etre valide
+        request()->validate([
+            'name' => 'required',
+            'hp' => 'required',
+            'degat' => 'required',
+            'defense' => 'required',
+            'esquive' => 'required',
+        ]);
+
+        $classe = Classe::create([
+           'name' => request('name'),
+           'hp_base' => request('hp'),
+           'hp_max' => request('hp'),
+           'hp_current' => request('hp'),
+           'degat_base' => request('degat'),
+           'degat_max' => request('degat'),
+           'degat_current' => request('degat'),
+           'defense_base' => request('defense'),
+           'defense_max' => request('defense'),
+           'defense_current' => request('defense'),
+           'esquive_base' => request('esquive'),
+           'esquive_max' => request('esquive'),
+           'esquive_current' => request('esquive'),
+        ]);
+
+        $classe->save();
+
+        return redirect(route('admin.users.index'));
     }
 
     /**
@@ -69,25 +97,25 @@ class ClasseController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @param Classe $classe
      * @return Response
      */
     public function update(Request $request, Classe $classe)
     {
         $classe->name = $request->name;
-        $classe->hp_base = $request->hp_base;
-        $classe->hp_max = $request->hp_max;
-        $classe->hp_current = $request->hp_current;
-        $classe->degat_base = $request->degat_base;
-        $classe->degat_max = $request->degat_max;
-        $classe->degat_current = $request->degat_current;
-        $classe->defense_base = $request->defense_base;
-        $classe->defense_max = $request->defense_max;
-        $classe->defense_current = $request->defense_current;
-        $classe->esquive_base = $request->esquive_base;
-        $classe->esquive_max = $request->esquive_max;
-        $classe->esquive_current = $request->esquive_current;
+        $classe->hp_base = $request->hp;
+        $classe->hp_max = $request->hp;
+        $classe->hp_current = $request->hp;
+        $classe->degat_base = $request->degat;
+        $classe->degat_max = $request->degat;
+        $classe->degat_current = $request->degat;
+        $classe->defense_base = $request->defense;
+        $classe->defense_max = $request->defense;
+        $classe->defense_current = $request->defense;
+        $classe->esquive_base = $request->esquive;
+        $classe->esquive_max = $request->esquive;
+        $classe->esquive_current = $request->esquive;
 
         $classe->save();
 
--- Laravel_Game/resources/views/admin/classe/create.blade.php
+++ Laravel_Game/resources/views/admin/classe/create.blade.php
@@ -8,35 +8,34 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header h3 text-center">Modifier la classe <span class="font-weight-bold">{{ $classe->name }}</span></div>
+                    <div class="card-header h3 text-center">Créer une classe</div>
 
                     <div class="card-body">
-                        <form action="{{ route('admin.classe.update', $classe) }}" method="POST">
+                        <form action="{{ route('admin.classe.store') }}" method="POST">
+                            @csrf
                             <ul class="list-group list-group-flush" style="list-style-type:none">
                                 <li class="list-group-item text-center">
-                                    <input id="name" name="name" class="form-control text-center mx-auto w-50" value="{{ $classe->name }}">
+                                    <input id="name" name="name" class="form-control text-center mx-auto w-50" value="{{ old('name') }}" placeholder="Nom">
                                 </li>
                                 <li class="list-group-item text-center">
                                     <i class="fas fa-heart mr-2"></i>
-                                    <input id="hp" name="hp" type="number" class="form-control text-center mx-auto w-25 d-inline" value="{{ $classe->hp_base }}">
+                                    <input id="hp" name="hp" type="number" class="form-control text-center mx-auto w-25 d-inline" value="{{ old('hp') }}" placeholder="Vie">
                                 </li>
                                 <li class="list-group-item text-center">
                                     <i class="fas fa-gavel mr-2"></i>
-                                    <input id="degat" name="degat" type="number" class="form-control text-center mx-auto w-25 d-inline" value="{{ $classe->degat_base }}">
+                                    <input id="degat" name="degat" type="number" class="form-control text-center mx-auto w-25 d-inline" value="{{ old('degat') }}" placeholder="Dégats">
                                 </li>
                                 <li class="list-group-item text-center">
                                     <i class="fas fa-shield-alt mr-2"></i>
-                                    <input id="defense" name="defense" type="number" class="form-control text-center mx-auto w-25 d-inline" value="{{ $classe->defense_base }}">
+                                    <input id="defense" name="defense" type="number" class="form-control text-center mx-auto w-25 d-inline" value="{{ old('defense') }}" placeholder="Défense">
                                 </li>
                                 <li class="list-group-item text-center">
                                     <i class="fas fa-walking mr-3"></i>
-                                    <input id="esquive" name="esquive" type="number" class="form-control text-center mx-auto w-25 d-inline" value="{{ $classe->esquive_base }}">
+                                    <input id="esquive" name="esquive" type="number" class="form-control text-center mx-auto w-25 d-inline" value="{{ old('esquive') }}" placeholder="Esquive">
                                 </li>
                             </ul>
-                            @csrf
-                            {{ method_field('PUT') }}
-                            <button type="submit" class="btn btn-warning btn-block mt-3 mx-auto font-weight-bold">
-                                Modifier
+                            <button type="submit" class="btn btn-success btn-block mt-3 mx-auto">
+                                Créer
                             </button>
                         </form>
                     </div>
